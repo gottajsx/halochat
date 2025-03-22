e06b8680aeb035918c8bbc1558fe00435bac749f
gottajsx <gottajsx@gmail.com>
2025-03-22 11:27:33 +0100

    Add ChatDetails component

--- components/ChatDetails.jsx
+++ components/ChatDetails.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import Loader from "./Loader";
+import Loader from "./Loader.jsx";
 import { AddPhotoAlternate } from "@mui/icons-material";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
